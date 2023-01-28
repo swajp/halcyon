186703039ef2c98f630e2d2a8e697ea73405b852
swajp <sondikemail@seznam.cz>
2023-01-28 17:37:16 +0100

    + založení projektu, loading formulář, login formulář (bez designu)

---
+++ LoadinForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Halcyon
+{
+    partial class LoadinForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelLoading = new System.Windows.Forms.Panel();
+            this.timerLoading = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(72)))), ((int)(((byte)(99)))));
+            this.panel1.Controls.Add(this.panelLoading);
+            this.panel1.Location = new System.Drawing.Point(1, 259);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(610, 23);
+            this.panel1.TabIndex = 0;
+            // 
+            // panelLoading
+            // 
+            this.panelLoading.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(210)))), ((int)(((byte)(145)))));
+            this.panelLoading.Location = new System.Drawing.Point(-8, 0);
+            this.panelLoading.Name = "panelLoading";
+            this.panelLoading.Size = new System.Drawing.Size(23, 29);
+            this.panelLoading.TabIndex = 1;
+            // 
+            // timerLoading
+            // 
+            this.timerLoading.Enabled = true;
+            this.timerLoading.Interval = 15;
+            this.timerLoading.Tick += new System.EventHandler(this.timerLoading_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(56)))), ((int)(((byte)(79)))));
+            this.ClientSize = new System.Drawing.Size(600, 280);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.No;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panelLoading;
+        private System.Windows.Forms.Timer timerLoading;
+    }
+}
---
+++ LoadinForm.cs
@@ -0,0 +1,25 @@
+namespace Halcyon
+{
+    public partial class LoadinForm : Form
+    {
+        public LoadinForm()
+        {
+            InitializeComponent();
+        }
+
+       private void timerLoading_Tick(object sender, EventArgs e)
+        {
+            panelLoading.Width += 3;
+
+            if(panelLoading.Width >= 650)
+            {
+                timerLoading.Stop();
+
+                this.Close();
+
+                LoginForm loginForm = new LoginForm();
+                loginForm.Show();
+            }
+        }
+    }
+}
