a5dc27da3cdf2df464fc60a28f99a6c96ea79e27
Miroslav Stejskal <Stejskal.Miroslav@stud.vassboskovice.cz>
2023-01-30 09:19:28 +0100

    + příprava pro databázi

---
+++ SqlRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    internal class SqlRepository
+    {
+        private string connectionStrng = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Halcyon;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
