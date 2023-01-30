f963a728183477bf5f53d742e6d91e8b89f95d28
Miroslav Stejskal <Stejskal.Miroslav@stud.vassboskovice.cz>
2023-01-30 09:19:14 +0100

    + třída UIActions

---
+++ UIActions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halcyon
+{
+    internal class UIActions : Form
+    {
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int LPAR);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        const int WM_NCLBUTTONDOWN = 0xA1;
+        const int HT_CAPTION = 0x2;
+        public void CloseApp()
+        {
+            Application.Exit();
+        }
+        public void MinimizeApp(Form form)
+        {
+            form.WindowState = FormWindowState.Minimized;
+        }
+        public void MoveApp(object sender, MouseEventArgs e, Form form)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(form.Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
